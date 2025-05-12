2dcfd4183040be2e41bff68292864e39711eed96
dima.moisieienko <dimonmail09@gmail.com>
2025-05-12 21:33:20 +0300

    introduction to React Router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      It is my homepage
+      <NavLink to={"/"}>Home</NavLink>
+      <NavLink to={"about"}>About</NavLink>
+      <NavLink to={"cart"}>Cart</NavLink>
+    </div>
+  );
+}
+
+export default Home;
