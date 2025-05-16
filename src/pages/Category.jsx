806698d3c346b2f8e177f30caa8f28848c5f1a8d
dima.moisieienko <dimonmail09@gmail.com>
2025-05-16 20:44:53 +0300

    worked with useLocation and useSearchParams

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,31 +1,51 @@
-import { Link, useParams, useSearchParams } from "react-router-dom";
+import { Link, useLocation, useParams, useSearchParams } from "react-router-dom";
 import { products } from "../data/data";
 import ProductNotFound from "../components/ProductNotFound";
 
 function Category() {
   const { id } = useParams();
+
+  const {pathname} = useLocation();
+  console.log(pathname);
+
   const [searchParams, setSearchParams] = useSearchParams();
-  const sort = searchParams.get("sort");// asc or desc
+  const sort = searchParams.get("sort"); // asc or desc
+  const maxPrice = searchParams.get("maxPrice")
+    ? Number(searchParams.get("maxPrice"))
+    : Infinity;
+
+  function handleChange(e) {
+    const value = e.target.value;
+
+    setSearchParams(value ? { maxPrice: value } : {});
+  }
 
   let categoryProducts = products.filter(
-    (product) => product.categoryId === id
-  )
-  
-  if(sort === "asc"){
-    categoryProducts.sort((a,b)=>a.price-b.price);
-  }
-  else if(sort === "desc"){
-    categoryProducts.sort((a,b)=>b.price-a.price);
-  }
+    (product) => product.categoryId === id && product.price <= maxPrice
+  );
 
-
-  if(!categoryProducts.length){
-    return <ProductNotFound/>
+  if (sort === "asc") {
+    categoryProducts.sort((a, b) => a.price - b.price);
+  } else if (sort === "desc") {
+    categoryProducts.sort((a, b) => b.price - a.price);
   }
 
   return (
     <div>
-      <h1>Category</h1> <input type="text" onChange={(e)=>{setSearchParams({sort: e.target.value})}}/>
+      <h1>Category</h1>
+      <div style={{ marginBottom: "15px" }}>
+        <label htmlFor="sort-price">Sort by price: </label>
+        <input
+          id="sort-price"
+          type="text"
+          onChange={(e) => {
+            setSearchParams({ sort: e.target.value });
+          }}
+        />
+      </div>
+
+      <label htmlFor="filter-max-price">Filter by max price:</label>
+      <input id="filter-max-price" type="text" onChange={handleChange} value={searchParams.get("maxPrice") || ""} />
       <ul style={{ display: "flex" }}>
         {categoryProducts.map((product) => (
           <li key={product.id}>
